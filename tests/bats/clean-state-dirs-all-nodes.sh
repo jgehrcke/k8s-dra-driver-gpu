b01b688afb7513dbae8164185a8b7d9ea27b9770
Dr. Jan-Philip Gehrcke <jgehrcke@nvidia.com>
2025-11-01 11:41:09 -0700

    tests: parallelize per-node state dir cleanup
    Signed-off-by: Dr. Jan-Philip Gehrcke <jgehrcke@nvidia.com>

--- tests/bats/clean-state-dirs-all-nodes.sh
+++ tests/bats/clean-state-dirs-all-nodes.sh
@@ -19,10 +19,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# TODO: think about wiping /var/run/cdi, too
+
 rm_kubelet_plugin_dirs_from_node () {
     local NODE_NAME="$1"
     echo "Run privileged pod to remove kubelet plugin directories on node ${NODE_NAME}"
-    kubectl run privpod-rm-plugindirs \
+    kubectl run "privpod-rm-plugindirs-${NODE_NAME}" \
         --rm \
         --image=busybox \
         --attach \
@@ -32,7 +34,8 @@ rm_kubelet_plugin_dirs_from_node () {
         "spec": {
             "nodeName": "'"${NODE_NAME}"'",
             "containers": [{
-            "name": "privpod-rm-plugindirs",
+            "name": "privpod-rm-plugindirs-'"${NODE_NAME}"'",
+            "metadata": {"labels": {"env": "batssuite"}},
             "image": "busybox",
             "securityContext": { "privileged": true },
             "volumeMounts": [{
@@ -52,5 +55,7 @@ rm_kubelet_plugin_dirs_from_node () {
 # Would be faster when using a daemonset. However, the output is more readable
 # when running sequentially.
 for node in $(kubectl get nodes -o jsonpath='{.items[*].metadata.name}'); do
-    rm_kubelet_plugin_dirs_from_node $node
+    rm_kubelet_plugin_dirs_from_node "$node" &
 done
+wait
+echo "state dir cleanup: done"
