041f4adba5acf1c34735cf939985e72823675b00
Dr. Jan-Philip Gehrcke <jgehrcke@nvidia.com>
2025-11-26 14:17:04 +0000

    Set version to v25.8.1
    Signed-off-by: Dr. Jan-Philip Gehrcke <jgehrcke@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -18,7 +18,7 @@ MODULE := github.com/NVIDIA/$(DRIVER_NAME)
 
 REGISTRY ?= nvcr.io/nvidia
 
-VERSION  ?= v25.8.0
+VERSION  ?= v25.8.1
 
 # vVERSION represents the version with a guaranteed v-prefix
 # Note: this is probably not consumed in our build chain.
